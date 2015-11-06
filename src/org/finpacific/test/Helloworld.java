bd51932d7247d50480f45130b9a086a2d2cdd9b9
ansonye0302 <anson.ye@finpacific.com>
2015-11-06 16:05:11 +1100

    Anson Test1
    Test HelloWorld.java

--- src/org/finpacific/test/Helloworld.java
+++ src/org/finpacific/test/Helloworld.java
@@ -7,6 +7,8 @@ public class Helloworld {
 		System.out.println("This is an updated file.");
 		System.out.println("This is a third line");
 		System.out.println("This is yet another line.");
+		
+		System.out.println("Anson Test.");
 	}
 
 }
--- src/org/finpacific/test/Test2File.java
+++ src/org/finpacific/test/Test2File.java
@@ -4,6 +4,8 @@ public class Test2File {
 
 	public static void main(String[] args) {
 		System.out.println("This is hello from the second file.");
+		
+		System.out.println("Anson Test 2");
 	}
 
 }
